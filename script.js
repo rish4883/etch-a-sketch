2c546f9d01a83ea4c9b9ca326e142cb83b442d1d
Rishabh <rishabh4883@gmail.com>
2022-07-28 18:46:58 +0530

    Add hover effect to the grid

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ for(i=0; i<16; i++) {
 
 const container = document.querySelector('.container');
 
+//append node to DOM
 grid.forEach( square => {
     container.appendChild(square);
+});
+
+// Adding hover event listener
+function handleMouseHover(e) {
+    e.target.classList.add('hover');
+}
+
+grid.forEach( square => {
+    square.addEventListener('mouseover', handleMouseHover);
+});
+
+// Adding leave event
+function handleMouseLeave(e) {
+    e.target.classList.remove('hover');
+}
+
+grid.forEach( square => {
+    square.addEventListener('mouseout', handleMouseLeave);
 });
