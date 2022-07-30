57d80a198a7e277df5a979a57866f197544cb976
Rishabh <rishabh4883@gmail.com>
2022-07-30 08:01:22 +0530

    Add Etch a sketch board with most features

--- script.js
+++ script.js
@@ -1,29 +1,99 @@
-const grid = [];
-for(i=0; i<16; i++) {
-    grid[i] = document.createElement("div");
-}
-
 const container = document.querySelector('.container');
 
-//append node to DOM
-grid.forEach( square => {
-    container.appendChild(square);
-});
+const grid = [];
 
-// Adding hover event listener
-function handleMouseHover(e) {
-    e.target.classList.add('hover');
+// Default Values
+createGrid(16);
+setSquareSize(16);
+addGrid();
+draw();
+
+// Function to create the grid squares
+function createGrid(n)  {
+    for(i=0; i<n**2; i++) {
+        grid[i] = document.createElement("div");
+    }
 }
 
-grid.forEach( square => {
-    square.addEventListener('mouseover', handleMouseHover);
-});
-
-// Adding leave event
-function handleMouseLeave(e) {
-    e.target.classList.remove('hover');
+// Function to determine the square size
+function setSquareSize(n) {
+    let borderLength = 2 * n;
+    // let squareSize = (700 - borderLength)/n;
+    let squareSize = 700/n;
+    grid.forEach( square => {
+       square.style.cssText = `height: ${squareSize}px; width: ${squareSize}px;`;
+    });
 }
 
-grid.forEach( square => {
-    square.addEventListener('mouseout', handleMouseLeave);
+// Function to append grid to DOM
+function addGrid() {
+    grid.forEach( square => {
+        container.appendChild(square);
+    });
+}
+
+// Function to remove node
+function removeGrid() {
+    grid.forEach( square => {
+        container.removeChild(square);
+    });
+    grid.length = 0;
+}
+
+// Slider value
+const slider = document.querySelector('.slider');
+
+// Grid slider label
+const gridSize = document.querySelector(".grid-size");
+
+slider.addEventListener('input', () => {
+    gridSize.textContent = `Grid size: ${slider.value} x ${slider.value}`;
+    removeGrid();
+    createGrid(slider.value);
+    setSquareSize(slider.value);
+    addGrid();
+    draw();
 });
+
+ 
+// draw Button Event
+const drawButton = document.querySelector('.draw');
+// Event Listener for drawing
+function draw() {
+    grid.forEach( square => {
+        square.addEventListener('mouseover', (e) => { 
+            e.target.classList.remove('erase');
+            e.target.classList.add('click');
+        });
+    });
+}
+
+drawButton.addEventListener('click', draw);
+
+// Clear button event
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+    grid.forEach(square => square.classList.remove('click'))
+});
+
+// Eraser Button event
+const eraser = document.querySelector('.eraser');
+
+function handleEraser() {
+    grid.forEach( square => {
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.remove('click');
+            e.target.classList.add('erase');
+        });
+    });
+}
+
+eraser.addEventListener('click', handleEraser);
+
+// Color Picker
+const color = document.querySelector('#pick-color');
+
+
+
+
